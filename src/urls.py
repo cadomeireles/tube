cd4dd4e1533da0cdfb932a0450a55efd861ee76b
Ricardo Meireles <cadomeireles@gmail.com>
2017-07-21 22:30:17 -0300

    Include videos URLconf to root URLconf

--- src/urls.py
+++ src/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+
+from .videos import urls as videos_urls
 
 
 urlpatterns = [
+    # Videos URLs
+    url(r'^videos/', include(videos_urls, namespace='videos')),
+
     url(r'^admin/', admin.site.urls),
 ]
